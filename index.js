8c331b24f70f9439821f0b4f34027911a4e962a7
Dhruvit-Sattvabit <161458048+dhruvit-sattvabit@users.noreply.github.com>
2024-10-18 16:54:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const moment = require('moment-timezone');
 
 function logWithTimezone(message) {
-    const timestamp = moment().tz('Asia/Kolkata').format('MM/DD/YYYY, h:mm:ss A');
+    const timestamp = moment().tz('Asia/Kolkata').format('DD/MM/YYYY, h:mm:ss A');
     console.log(`${timestamp}: ${message}`);
 }
 
